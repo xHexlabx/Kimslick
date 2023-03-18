b81949c92e65a5c96cb7b5e0ea5cc971dcb06cba
Meaw_zap <118115127+xHexlabx@users.noreply.github.com>
2023-03-18 10:52:29 +0700

    .

---
+++ index.js
@@ -0,0 +1,24 @@
+const { Client, IntentsBitField, Partials } = require("discord.js");
+const WOK = require("wokcommands");
+const path = require("path");
+require("dotenv/config");
+
+const client = new Client({
+  intents: [
+    IntentsBitField.Flags.Guilds,
+    IntentsBitField.Flags.GuildMessages,
+    IntentsBitField.Flags.DirectMessages,
+    IntentsBitField.Flags.MessageContent,
+  ], 
+  partials: [Partials.Channel],
+});
+
+client.on("ready", () => {
+  console.log("ready!");
+  new WOK({
+    client,
+    commandsDir: path.join(__dirname, "commands"),
+  });
+});
+
+client.login(process.env.TOKEN);
